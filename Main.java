8d2fd3c78c9ce91f8fc0f0bf6f8e7b3b55a3a17f
FuryX625 <serafin.m625@gmail.com>
2022-04-05 09:55:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner wczytanie=new Scanner(System.in);
+    char[] tekst;
+    int i=0;
+    System.out.println("Wpisuj znaki do kopiowania, jeżeli chcsz zakończyć podj q");
+    do{
+      tekst[i]=wczytanie.next.toCharArray;
+    } while(tekst[i]!='q');
+    
+  }
+}
