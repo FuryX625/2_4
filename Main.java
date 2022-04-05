704f3c942b96dcc15556d67d405f6a65979d6946
serafin625 <serafin.m625@gmail.com>
2022-04-05 09:59:14 +0000

    1

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner wczytanie=new Scanner(System.in);
-    char[] tekst;
-    int i=0;
+    char z='z';
+    
     System.out.println("Wpisuj znaki do kopiowania, jeżeli chcsz zakończyć podj q");
-    do{
-      tekst[i]=wczytanie.next.toCharArray;
-    } while(tekst[i]!='q');
+    while(true){
+      z=wczytanie.next().charAt(0);
+      if (z=='q') break;
+      System.out.println(z);
+    }
+    
     
   }
 }
