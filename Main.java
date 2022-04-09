d159ec08b053b149a0a901f74bfebdeb212c9ffb
serafin625 <serafin.m625@gmail.com>
2022-04-09 18:04:29 +0000

    leak from scanner deleted

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    
     Scanner wczytanie=new Scanner(System.in);
-    char z='z';
+    char z=' ';
     
     System.out.println("Wpisuj znaki do kopiowania, jeżeli chcsz zakończyć podj q");
     while(true){
       z=wczytanie.next().charAt(0);
       if (z=='q') break;
       System.out.println(z);
-    }
-    
-    
+      
+    } 
+    wczytanie.close();
   }
 }
